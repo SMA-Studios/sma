4a8b4ff7647b0929cca124df99677cd9320da5c1
Adam Waldie <aaw4421@rit.edu>
2022-04-12 15:41:06 -0400

    Extra stuff

--- src/Boys.tsx
+++ src/Boys.tsx
@@ -2,20 +2,24 @@ import {
   Box,
   createStyles,
   Grid,
+  Hidden,
   makeStyles,
   Paper,
   Theme,
 } from "@material-ui/core";
 import React from "react";
+import { callbackify } from "util";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      margin: 0,
+      overflow: "hidden",
     },
     paper: {
-      width: 420,
-      height: 300,
+      width: "calc(100vw/6)",
+      height: 100,
     },
   })
 );
@@ -24,21 +28,21 @@ const Boys = () => {
   const classes = useStyles();
 
   return (
-    <Box display="flex" width={1260} height={900}>
+    <Box display="flex" alignItems="center" justifyContent="center" width={"100vw"} height={"100vh"}>
       <Grid container spacing={3}>
         <Grid item xs={4}>
-          <Box display="flex" justifyContent="center">
-            <Paper className={classes.paper}>a</Paper>
+          <Box display="flex" textAlign="center" justifyContent={"center"}>
+            <Paper className={classes.paper}>peepoSit</Paper>
           </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box display="flex" justifyContent="center">
-            <Paper className={classes.paper}>b</Paper>
+          <Box display="flex" textAlign="center" justifyContent={"center"}>
+            <Paper className={classes.paper}>peepoVibetown</Paper>
           </Box>
         </Grid>
         <Grid item xs={4}>
-          <Box display="flex" justifyContent="center">
-            <Paper className={classes.paper}>c</Paper>
+          <Box display="flex" textAlign="center" justifyContent={"center"}>
+            <Paper className={classes.paper}>peepoBoggerton</Paper>
           </Box>
         </Grid>
       </Grid>
--- src/index.tsx
+++ src/index.tsx
@@ -8,3 +8,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+document.body.style.margin = "0";
+document.body.style.overflow = "hidden";
